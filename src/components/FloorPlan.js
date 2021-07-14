6f106b642cd0a69b2373367908aa3f64d8f1c332
brendalima <brendalima@>
2021-07-14 19:54:49 -0300

    Initial commit. Building app structure

---
+++ src/components/FloorPlan.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function FloorPlan() {
+  return (
+    <div>
+      FloorPlan
+    </div>
+  )
+}
+
+export default FloorPlan;
---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../components/Header';
+import FloorPlan from '../components/FloorPlan';
+
+function Home() {
+  return (
+    <div>
+      <Header />
+      <FloorPlan />
+    </div>
+  )
+}
+
+export default Home;
