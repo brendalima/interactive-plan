d85a5f00a8a57ecba42ba6ad61c258db243e4752
brendalima <brendalima@>
2021-07-15 00:41:55 -0300

    Adjusting the person scale and making it draggable

--- src/components/Person.js
+++ src/components/Person.js
@@ -1,14 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import useImage from 'use-image';
 import { Layer, Image } from 'react-konva';
+import { useSelector } from 'react-redux';
 import person from '../images/person.svg';
 
 function Person() {
   const [image] = useImage(person);
+  // const state = useSelector(({ floorPlan }) => floorPlan);
+  // const { realWidth, realHeight } = state;
+  const [personWidth, setPersonWidth] = useState(50);
+  const [personThickness, setPersonThickness] = useState(30);
+
+  const calculateScale = () => {
+    const scaledWidth = (500 * personWidth) / 617;
+    const scaledHeight = (500 * personThickness) / 610;
+    setPersonThickness(scaledHeight);
+    setPersonWidth(scaledWidth);
+  }
+
+  useEffect(() => {
+    calculateScale();
+  }, []);
+  
+  console.log(personWidth)
 
   return (
     <Layer>
-      <Image image={image} />
+      <Image image={image} width={personWidth} height={personThickness} draggable />
     </Layer>
   )
 }
