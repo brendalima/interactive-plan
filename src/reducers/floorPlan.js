cc796d45e41396e8a8055582dab6f53ad6223d3b
brendalima <brenda.c.lima@poli.ufrj.br>
2021-07-16 20:09:08 -0300

    adding custom real person dimentions to global store

--- src/reducers/floorPlan.js
+++ src/reducers/floorPlan.js
@@ -1,16 +1,21 @@
 import {
-  SET_REAL_DIMENTIONS
+  SET_REAL_DIMENTIONS,
+  SET_PERSON_ENTRY
 } from '../actions/floorPlan';
 
 const INITIAL_STATE = {
   realWidth: '',
   realHeight: '',
+  personWidthEntry: 50,
+  personThicknessEntry: 30,
 };
 
 const floorPlan = (state = INITIAL_STATE, action) => {
   switch (action.type) {
   case SET_REAL_DIMENTIONS:
     return { ...state, realWidth: action.width, realHeight: action.height };
+  case SET_PERSON_ENTRY:
+    return { ...state, personWidthEntry: action.width, personThicknessEntry: action.thickness };
   default:
     return state;
   }
