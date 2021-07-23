d9b74157d5c9c311ae6038c4701fa7bfd72248a8
brendalima <brenda.c.lima@poli.ufrj.br>
2021-07-23 15:33:26 -0300

    mocking floorplan data json response

---
+++ src/aux/data.js
@@ -0,0 +1,7 @@
+export const data = {
+  url: 'https://github.com/brendalima/interactive-plan/blob/master/src/images/floorplan.png',
+  width: 1565,
+  height: 1555,
+  realWidth: 617,
+  realHeight: 610,
+}
